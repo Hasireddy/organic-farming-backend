27402ffe1c46f071d399b308aaff6f1d7eba02cb
Hasireddy <malikireddy.uma@gmail.com>
2022-06-11 11:32:21 +0200

    changes made to auth.js

---
+++ controllers/auth.js
@@ -0,0 +1,27 @@
+const Farmer = require('../models/Farmer-details.js');
+const asyncHandler = require('../utils/asyncHandler.js');
+const ErrorResponse = require('../utils/ErrorResponse.js');
+
+const registerFarmer = asyncHandler(async(req, res, next) => {
+    /*
+    validate the input =>use a midddlewrae with joi
+    check if farmer already exists=>farmer.find(by email)
+    if exists say no=>throw ErrorResponse
+    if not exists create
+     Hash(and salt)the password
+    create Token jsonWebToken https://www.npmjs.com/package/jsonwebtoken
+    send Token => res.json() res.set() res.cookie()
+
+    */
+    res.json({ success: 'Farmer registered' });
+});
+
+const loginFarmer = asyncHandler(async(req, res, next) => {
+    const farmers = await post.find();
+    res.json({ success: "user registered" });
+});
+
+const getFarmer = asyncHandler(async(req, res, next) => {
+    const farmers = await post.find();
+    res.json({ success: "user registered" });
+});
