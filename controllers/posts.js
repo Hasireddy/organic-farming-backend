3c3046e01865cfeb918d2c9776cb6ea72843f38e
Hasireddy <malikireddy.uma@gmail.com>
2022-06-19 09:57:30 +0200

    Routers and VerifyToken added

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Farmer = require('../models/Farmer-details.js');
+const Farmer = require('../models/Farmer-schema.js');
 const asyncHandler = require('../utils/asyncHandler.js');
 const ErrorResponse = require('../utils/ErrorResponse.js');
 
@@ -9,4 +9,41 @@ const getAllPosts = asyncHandler(async (req, res, next) => {
     res.json(posts);
 });
 
-module.exports = { getAllPosts };
+
+const createPost = asyncHandler(async (req, res, next) => {
+    const { body } = req;
+    const newFarmer = await (await Farmer.create({ ...body }));
+    res.status(201).json(newFarmer);
+});
+
+const getSinglePost = asyncHandler(async (req, res, next) => {
+    // const { id } = req.params.id;
+    const farmer = await Farmer.findById(req.params.id);
+    if (!farmer) throw new ErrorResponse(`farmer with id of ${req.params.id} doesn't exist`, 404);
+    res.json(farmer);
+});
+
+const updatePost = asyncHandler(async (req, res, next) => {
+
+    const found = await Farmer.findById(req.params.id);
+    console.log(found);
+    if (!found) throw new ErrorResponse(`farmer with id of ${req.params.id} doesn't exist`, 404);
+    const updatedPost = await (
+        await Farmer.findOneAndUpdate(req.params.id, req.body, { new: true }));
+    res.json(updatedPost);
+});
+
+
+const deletePost = asyncHandler(async (req, res, next) => {
+
+    const found = await Farmer.findById(req.params.id);
+    console.log(found);
+    if (!found) throw new ErrorResponse(`farmer with id of ${req.params.id} doesn't exist`, 404);
+    const deleteddPost = await (
+        await Farmer.findByIdAndDelete(req.params.id));
+    res.json({ success: `Post with id of ${req.params.id} was deleted` });
+});
+
+
+
+module.exports = { getAllPosts, createPost, getSinglePost, updatePost, deletePost };
--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -2,11 +2,14 @@ const Router = require('express');
 // const router = require('express').Router();
 const verifyToken = require('../middlewares/verifyToken.js');
 const validateJOI = require('../middlewares/validateJOI.js');
-const { getAllPosts } = require('../controllers/posts.js');
+const { farmerSchema } = require('../joi/schema.js');
+const { getAllPosts, createPost, getSinglePost, updatePost, deletePost } = require('../controllers/posts.js');
 
 const postsRouter = Router();
 
-postsRouter.route('/').get(getAllPosts)
+postsRouter.route('/').get(getAllPosts).post(verifyToken, validateJOI(farmerSchema), createPost);
+
+postsRouter.route('/:id').get(getSinglePost).put(verifyToken, updatePost).delete(verifyToken, deletePost);
 
 
 module.exports = postsRouter;
