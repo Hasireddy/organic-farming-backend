7b74ebb7f56777f86de6c32ba74bce967521a00c
Hasireddy <malikireddy.uma@gmail.com>
2022-06-30 09:45:12 +0200

    data created

--- joi/schema.js
+++ joi/schema.js
@@ -22,7 +22,7 @@ const productSchema = Joi.object({
     ProductName: Joi.string().required(),
     Description: Joi.string().required(),
     Price: Joi.number().required(),
-    Category: Joi.string().alphanum().min(6).max(12).required(),
+    Category: Joi.string().alphanum().min(5).max(12).required(),
     Image: Joi.object()
 
 })
