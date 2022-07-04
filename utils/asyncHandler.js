b55a9558813f5fbea3e42ca164582d774a736b43
Hasireddy <malikireddy.uma@gmail.com>
2022-07-04 17:05:01 +0200

    file name changed

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (fn) => {
-    return function(req, res, next) {
+    return function (req, res, next) {
         return Promise.resolve(fn(req, res, next)).catch(next);
     };
 }
