1a4da1922664cde877aa70c4f6a82e3cab432256
Isaiah-Umuks <umukoroisaiah2@gmail.com>
2022-07-05 09:19:26 +0200

    no change

--- routes/postsRouter.js
+++ routes/postsRouter.js
@@ -1,15 +1,15 @@
-const Router = require('express');
-// const router = require('express').Router();
-const verifyToken = require('../middlewares/verifyToken.js');
-const validateJOI = require('../middlewares/validateJOI.js');
-const { farmerSchema } = require('../joi/schema.js');
-const { getAllPosts, createPost, getSinglePost, updatePost, deletePost } = require('../controllers/posts.js');
+// const Router = require('express');
+// // const router = require('express').Router();
+// const verifyToken = require('../middlewares/verifyToken.js');
+// const validateJOI = require('../middlewares/validateJOI.js');
+// const { farmerSchema } = require('../joi/schema.js');
+// const { getAllPosts, createPost, getSinglePost, updatePost, deletePost } = require('../controllers/posts.js');
 
-const postsRouter = Router();
+// const postsRouter = Router();
 
-postsRouter.route('/').get(getAllPosts).post(verifyToken, validateJOI(farmerSchema), createPost);
+// postsRouter.route('/').get(getAllPosts).post(verifyToken, validateJOI(farmerSchema), createPost);
 
-postsRouter.route('/:id').get(getSinglePost).put(verifyToken, updatePost).delete(verifyToken, deletePost);
+// postsRouter.route('/:id').get(getSinglePost).put(verifyToken, updatePost).delete(verifyToken, deletePost);
 
 
-module.exports = postsRouter;
+// module.exports = postsRouter;
